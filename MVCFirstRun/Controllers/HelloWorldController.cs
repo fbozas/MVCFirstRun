8cf1393d9165367f680458d2c7bd0a3c949ad28e
Panos Bozas <fbozas@hotmail.com>
2022-06-01 20:23:10 +0300

    Add first controller and method (string)

---
+++ MVCFirstRun/Controllers/HelloWorldController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace MVCFirstRun.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "Hello World";
+        }
+    }
+}

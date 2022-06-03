9687b8d0f17fea5827ac70164893ad52711fa8b2
Panos Bozas <fbozas@hotmail.com>
2022-06-03 18:47:59 +0300

    ViewBag ViewData and TempData

---
+++ MVCFirstRun/Controllers/StateManagementController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace MVCFirstRun.Controllers
+{
+    public class StateManagementController : Controller
+    {
+        // GET: StateManagement
+        public ActionResult Index()
+        {
+            ViewBag.Name = "Kostas";
+            ViewData["Age"] = 33;
+            TempData["Message"] = "Your message";
+
+            //return View();
+            return RedirectToAction("Destination");
+        }
+
+        public ActionResult Destination()
+        {
+            return View();
+        }
+    }
+}

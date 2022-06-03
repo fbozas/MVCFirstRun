01a7768413397e72407d7560c5cda73c07672bfa
Panos Bozas <fbozas@hotmail.com>
2022-06-03 20:31:52 +0300

    Form Submission Techniques

---
+++ MVCFirstRun/Controllers/FormDemoController.cs
@@ -0,0 +1,59 @@
+﻿using MVCFirstRun.Models;
+using System;
+using System.Web.Mvc;
+
+namespace MVCFirstRun.Controllers
+{
+    public class FormDemoController : Controller
+    {
+        // GET: FormDemo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Calculate()
+        {
+            string fullName = Request.Form["fullName"].ToString();
+            DateTime dateOfBirth = Convert.ToDateTime(Request.Form["dateOfBirth"]);
+
+            int age = DateTime.Now.Year - dateOfBirth.Year;
+            int ageNextYear = age + 1;
+
+            ViewBag.FullName = fullName;
+            ViewBag.Age = age;
+            ViewBag.AgeNextYear = ageNextYear;
+
+            return View("Index");
+        }
+
+        [HttpPost]
+        public ActionResult CalculateTwo(string fullNameTwo, string dateOfBirthTwo)
+        {
+            string fullName = fullNameTwo;
+            DateTime dateOfBirth = Convert.ToDateTime(dateOfBirthTwo);
+
+            int age = DateTime.Now.Year - dateOfBirth.Year;
+            int ageNextYear = age + 1;
+
+            ViewBag.FullName = fullName;
+            ViewBag.Age = age;
+            ViewBag.AgeNextYear = ageNextYear;
+
+            return View("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Person person)
+        {
+            return View("CreatePost", person);
+        }
+    }
+}
---
+++ MVCFirstRun/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MVCFirstRun.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public int Age 
+        {
+            get => DateTime.Now.Year - DateOfBirth.Year; 
+        }
+
+        public int AgeNextYear
+        {
+            get => Age + 1;
+        }
+    }
+}

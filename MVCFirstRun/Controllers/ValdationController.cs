b99d3f6f99d6e735ed60d9e3a45d03a4467644a4
Panos Bozas <fbozas@hotmail.com>
2022-06-06 18:55:26 +0300

    Validation: Initial

---
+++ MVCFirstRun/Controllers/ValdationController.cs
@@ -0,0 +1,31 @@
+﻿using MVCFirstRun.Models;
+using System.Web.Mvc;
+
+namespace MVCFirstRun.Controllers
+{
+    public class ValdationController : Controller
+    {
+        // GET: Valdation
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                return Content("Success!");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ MVCFirstRun/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCFirstRun.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        [StringLength(15, ErrorMessage = "UserName cannot be more than 15 characters")]
+        public string UserName { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfrimPassword { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public int PostalCode { get; set; }
+        public string Profile { get; set; }
+        public string Photo { get; set; }
+        public string AddtionalComments { get; set; }
+        public string City { get; set; }
+    }
+}

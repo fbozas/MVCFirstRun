18a1e2bcbbe31ff3bb3faa54f500624aedbe2da2
Panos Bozas <fbozas@hotmail.com>
2022-06-09 19:09:54 +0300

    Antiforgery Token explained

---
+++ MVCFirstRun/Controllers/AttackController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+
+namespace MVCFirstRun.Controllers
+{
+    public class AttackController : Controller
+    {
+        // GET: Attack
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CheckLoginCredentials()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CheckLoginCredentials(string UserName, string Password)
+        {
+            if(UserName.Equals("takis123") && Password.Equals("123"))
+            {
+                return View("Index");
+            }
+            return View();
+        }
+    }
+}
